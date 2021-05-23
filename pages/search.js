1dde972cc94e938e6f10cbd5c84a61bfb9676280
Rahul rai <46862628+Rahulraifzb@users.noreply.github.com>
2021-05-23 09:35:52 -0700

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import SearchResults from "../components/SearchResults";
 
 function search({results}) {
     const router = useRouter()
-    console.log(results)
+    
     return (
         <div>
             <head>
